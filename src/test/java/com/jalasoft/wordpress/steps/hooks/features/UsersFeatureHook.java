0b8752ed8ecb64cd1223169da18cd32bf02768b2
Sergio Mendieta Valenzuela <Sergio.Mendieta@fundacion-jala.org>
2022-07-10 01:41:13 -0400

    T153-GET_Status_By_Invalid_Name_Subscriber added

---
+++ src/test/java/com/jalasoft/wordpress/steps/hooks/features/UsersFeatureHook.java
@@ -0,0 +1,52 @@
+package com.jalasoft.wordpress.steps.hooks.features;
+
+import api.http.HttpResponse;
+import api.methods.APIUsersMethods;
+import constants.DomainAppEnums;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.restassured.response.Response;
+import org.testng.Assert;
+
+import java.util.Objects;
+
+public class UsersFeatureHook {
+    private final HttpResponse response;
+    private String userId;
+
+    public UsersFeatureHook(HttpResponse response) {
+        this.response = response;
+    }
+
+    @Before("@RetrieveAUser or @RetrieveMe or @UpdateUser or @UpdateMe or @DeleteAUser or @DeleteMe")
+    public void beforeRetrieveAUserFeature() {
+        Response requestResponse = APIUsersMethods.createAUser(DomainAppEnums.UserRole.ADMINISTRATOR.getUserRole());
+
+        if (Objects.nonNull(requestResponse)) {
+            response.setResponse(requestResponse);
+        } else {
+            Assert.fail("User was not created");
+        }
+        userId = response.getResponse().jsonPath().getString("id");
+    }
+
+    @Before("@GetStatusByNameSubscriberUser")
+    public void beforeTestsWithSubscriberUser() {
+        Response requestResponse = APIUsersMethods.createAUser(DomainAppEnums.UserRole.SUBSCRIBER.getUserRole());
+
+        if (Objects.nonNull(requestResponse)) {
+            response.setResponse(requestResponse);
+        } else {
+            Assert.fail("User was not created");
+        }
+        userId = response.getResponse().jsonPath().getString("id");
+
+    }
+
+    @After("@CreateUser or @RetrieveAUser or @UpdateUser or @RetrieveMe or @UpdateMe or @GetStatusByNameSubscriberUser")
+    public void afterCreateAUserFeature() {
+        String status = APIUsersMethods.deleteUserById(userId);
+
+        Assert.assertEquals(status, "true", "User was not deleted");
+    }
+}
