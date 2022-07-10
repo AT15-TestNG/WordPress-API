a87af0f4e2767987f5f83db3a9f23518a942bf2e
net_creature <agustinmediotti@gmail.com>
2022-07-10 17:11:59 -0300

    added @CreateAPageAsSub, @RetrieveAPageAsSub, @DeleteAPageAsSub, @UpdateAPageAsSub, delete page with force

--- src/test/java/com/jalasoft/wordpress/steps/hooks/features/PagesFeatureHook.java
+++ src/test/java/com/jalasoft/wordpress/steps/hooks/features/PagesFeatureHook.java
@@ -12,12 +12,13 @@ import java.util.Objects;
 
 public class PagesFeatureHook {
     private final HttpResponse response;
+    private String pageId;
 
     public PagesFeatureHook(HttpResponse response) {
         this.response = response;
     }
 
-    @Before("@RetrieveAPage or @UpdateAPage or @DeleteAPage")
+    @Before("@RetrieveAPage or @UpdateAPage or @DeleteAPage or @CreateAPageAsSub or @RetrieveAPageAsSub or @UpdateAPageAsSub or @DeleteAPageAsSub")
     public void beforeRetrieveAPageFeature() {
         String content = "TestNG WordPress Page content";
         String title = "TestNG WordPress Page title";
@@ -30,13 +31,19 @@ public class PagesFeatureHook {
         } else {
             Assert.fail("page was not created");
         }
+        pageId = response.getResponse().jsonPath().getString("id");
     }
 
     @After("@CreateAPage or @RetrieveAPage or @UpdateAPage")
     public void afterCreateAPageFeature() {
         String id = response.getResponse().jsonPath().getString("id");
-        String status = APIPagesMethods.deletePageById(id);
+        boolean status = APIPagesMethods.deletePageById(id);
+        Assert.assertTrue(status, "Page was not deleted");
+    }
 
-        Assert.assertEquals(status, "trash", "page was not deleted");
+    @After("@CreateAPageAsSub or @RetrieveAPageAsSub or @UpdateAPageAsSub")
+    public void afterCreateAPageFeatureAsSub() {
+        boolean status = APIPagesMethods.deletePageById(pageId);
+        Assert.assertTrue(status, "Page was not deleted");
     }
 }
