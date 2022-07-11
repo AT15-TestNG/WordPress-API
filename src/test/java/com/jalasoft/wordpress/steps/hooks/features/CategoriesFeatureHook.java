9aa35d9b7a2134ea06532d37b38164e485dddd0c
Adrian Oviedo <60976167+adrianovi@users.noreply.github.com>
2022-07-11 09:35:48 -0400

    Added scenarios for CRUD Categories as Subscriber (#51)

--- src/test/java/com/jalasoft/wordpress/steps/hooks/features/CategoriesFeatureHook.java
+++ src/test/java/com/jalasoft/wordpress/steps/hooks/features/CategoriesFeatureHook.java
@@ -11,22 +11,32 @@ import java.util.Objects;
 
 public class CategoriesFeatureHook {
     private final HttpResponse response;
+    private String categoryId;
 
     public CategoriesFeatureHook(HttpResponse response) {
         this.response = response;
     }
 
-    @After("@CreateACategory or @RetrieveACategory or @UpdateACategory")
+    @After("@CreateACategory or @RetrieveACategory or @UpdateACategory or @RetrieveACategoryAsSubscriber or @UpdateACategoryAsSubscriber or @DeleteACategoryAsSubscriber")
     public void afterCreateACategory() {
-        String id = response.getResponse().jsonPath().getString("id");
-        boolean deleted = APICategoriesMethods.deleteCategoryById(id);
+        if (Objects.nonNull(response.getResponse().jsonPath().getString("id"))) {
+            String id = response.getResponse().jsonPath().getString("id");
+            boolean deleted = APICategoriesMethods.deleteCategoryById(id);
 
-        Assert.assertTrue(deleted, "Category was not deleted");
+            Assert.assertTrue(deleted, "Category was not deleted");
+        } else if (Objects.nonNull(categoryId)) {
+            boolean deleted = APICategoriesMethods.deleteCategoryById(categoryId);
+
+            Assert.assertTrue(deleted, "Category was not deleted");
+        } else {
+            Assert.fail("Category was not able to being deleted");
+        }
     }
 
-    @Before("@RetrieveACategory or @UpdateACategory or @DeleteACategory or @DeleteACategoryError501")
+    @Before("@RetrieveACategory or @UpdateACategory or @DeleteACategory or @DeleteACategoryError501 or @RetrieveACategoryAsSubscriber or @UpdateACategoryAsSubscriber or @DeleteACategoryAsSubscriber")
     public void beforeRetrieveACategory() {
         Response requestResponse = APICategoriesMethods.createACategory();
+        categoryId = requestResponse.jsonPath().getString("id");
 
         if (Objects.nonNull(requestResponse)) {
             response.setResponse(requestResponse);
