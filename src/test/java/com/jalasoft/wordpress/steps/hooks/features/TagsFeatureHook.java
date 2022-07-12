2a191eaded007f04d333caba3d8917e39afa6ab7
Jimy Tastaca <jimy.tastaca@fundacion-jala.org>
2022-07-12 01:22:15 -0400

    added tags CRUD operations as Subscriber

--- src/test/java/com/jalasoft/wordpress/steps/hooks/features/TagsFeatureHook.java
+++ src/test/java/com/jalasoft/wordpress/steps/hooks/features/TagsFeatureHook.java
@@ -11,24 +11,32 @@ import java.util.Objects;
 
 public class TagsFeatureHook {
     private final HttpResponse response;
+    private String tagId;
 
     public TagsFeatureHook(HttpResponse response) {
         this.response = response;
     }
 
-    @After("@CreateATag or @RetrieveATag or @UpdateATag")
-    public void afterCreateACategory() {
-        String id = response.getResponse().jsonPath().getString("id");
-        boolean deleted = APITagsMethods.deleteTagById(id);
+    @After("@CreateATag or @RetrieveATag or @UpdateATag or @RetrieveATagAsSubscriber or @UpdateATagAsSubscriber or @DeleteATagAsSubscriber")
+    public void afterCreateATag() {
+        if (Objects.nonNull(response.getResponse().jsonPath().getString("id"))) {
+            String id = response.getResponse().jsonPath().getString("id");
+            boolean deleted = APITagsMethods.deleteTagById(id);
+            Assert.assertTrue(deleted, "Tag was not deleted");
+        } else if (Objects.nonNull(tagId)) {
+            boolean deleted = APITagsMethods.deleteTagById(tagId);
 
-        Assert.assertTrue(deleted, "Tag was not deleted");
+            Assert.assertTrue(deleted, "Tag was not deleted");
+        } else {
+            Assert.fail("Tag was not able to being deleted");
+        }
     }
-    @Before("@RetrieveATag or @UpdateATag or @DeleteATag or @DeleteATagError501")
+    @Before("@RetrieveATag or @UpdateATag or @DeleteATag or @DeleteATagError501 or @RetrieveATagAsSubscriber or @UpdateATagAsSubscriber or @DeleteATagAsSubscriber")
     public void beforeRetrieveATag() {
         String name = "Tag Name Example";
 
         Response requestResponse = APITagsMethods.createATag(name);
-
+        tagId = requestResponse.jsonPath().getString("id");
         if (Objects.nonNull(requestResponse)) {
             response.setResponse(requestResponse);
         } else {
